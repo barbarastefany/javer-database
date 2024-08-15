70e59aeb8eba5e8f33dbb406b4f3cc162875827e
barbarastefany <barbara.stefany@outlook.com.br>
2024-08-15 14:42:41 -0300

    fix

--- src/main/java/com/javerbank/javer_database/service/ClienteService.java
+++ src/main/java/com/javerbank/javer_database/service/ClienteService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 public class ClienteService {
 
-    private ClienteRepository clienteRepository;
+    private final ClienteRepository clienteRepository;
 
     @Autowired
     public ClienteService(ClienteRepository clienteRepository) {
